1852cda3ca0264cb2162c4e09c371dc6f1cfb96f
Professor Edson Cruz <39575202+profedsoncruz@users.noreply.github.com>
2018-10-01 16:11:05 -0300

    Updated file

--- gazeta-apuracao-front-test.sh
+++ gazeta-apuracao-front-test.sh
@@ -4,5 +4,5 @@ now=`date +"%m.%d.%Y-%H.%M.%S"`
 
 ROOT="https://apuracao.gazetadopovo.com.br";
 
-### Landing on Inbox All Requests Page
+### Landing on Gazeta do Povo Apuração
 $(npm bin)/sitespeed.io $ROOT --browsertime.headless --browsertime.timeouts.pageCompleteCheck 10000 --browsertime.pageCompleteCheck 'return (document.getElementsByClassName("c-sumario").length > 0)' -n 10 --outputFolder ../reports/"apuracao-gazeta-$now"
